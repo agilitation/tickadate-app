b18048ee1c96ccfaacb4f84a77dc7a10eb76d3e9
romainbessugesmeusy <romainbessuges@gmail.com>
2017-05-29 13:41:55 +0200

    I18N + file groups

--- tickadate/VisibleMonthTooltipView.swift
+++ tickadate/VisibleMonthTooltipView.swift
@@ -12,12 +12,5 @@ import DynamicColor
 class VisibleMonthTooltipView: UIView {
 
   var label:UILabel!
-      /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
 
 }
